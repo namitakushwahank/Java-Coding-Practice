6c98e6de9b036c835109ea272cbd8e1906dcd44f
namitakushwahank <namitakushwaha4@gmail.com>
2023-10-27 01:44:13 +0530

    git commit first

---
+++ src/main/java/org/javaForSelenium/FirstProgram.java
@@ -0,0 +1,9 @@
+package org.javaForSelenium;
+
+public class FirstProgram {
+    public static void main(String[] args) {
+        int age = 29;
+        String name = "Namita Kushwaha";
+        System.out.println("My name is " + name + " and my age is " + age);
+    }
+}
